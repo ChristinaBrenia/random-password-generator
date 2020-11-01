65c73e45a9ba4e7d823957f082a8a2d2ba22385c
Christina Brenia <ChristinaBrenia@Christinas-MBP.home>
2020-11-01 09:57:28 -0500

    Added else statements and uppercase conversions

--- Develop/script.js
+++ Develop/script.js
@@ -14,16 +14,35 @@ number = [1,2,3,4,5,6,7,8,9];
 //Lower Case 
 lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]; 
 
+//Functions turns lowerCase to upperCase
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+//Creates uppercase from lowercase letters
+upperCase = lowerCase.map(toUpper);
 
 //prompts for password count
 enter = parseInt(prompt("How many characters will your password be? Must be between 8 and 128"))
+//if less than 8 or over 128 error or no enter error
+if (!enter) {
+  alert("Your password must have a value")
 
+}else if (enter <= 7 || enter > 128) {
+  enter = parseInt(prompt('You must enter a value between 8 and 128'));
+}else {
+//prompts for number, special character, uppercase, lowercase when user enters corrent value
+confirmNumber = confirm('Will your password contain numbers?');
+confirmCharacter = confirm('Will your password contain special characters?');
+confirmUppercase = confirm('Will your password contain Uppercase letters?');
+confirmLowercase = confirm('Will your password contain lowercase letters?');
+};
 
-//prompts for number, special character, uppercase, lowercase
-  confirmNumber = confirm('Will your password contain numbers?');
-  confirmCharacter = confirm('Will your password contain special characters?');
-  confirmUppercase = confirm('Will your password contain Uppercase letters?');
-  confirmLowercase = confirm('Will your password contain lowercase letters?')
+//Didnt Confirm character, confirm number, or confirm uppercase, or confirm lowercase - did not meet requirement 
+if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+  choices = alert('You must choose a criteria to set a password')
+}
+
+//
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
